bb9635053ea02fa39995e6d09890ca21c94a3b4e
hatelove <jay.hatelove@gmail.com>
2017-04-07 15:21:19 +0800

    complete demo

--- GoogleSearchWebTesting/UnitTest1.cs
+++ GoogleSearchWebTesting/UnitTest1.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using FluentAutomation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GoogleSearchWebTesting
 {
     [TestClass]
-    public class UnitTest1
-    {        
+    public class UnitTest1 : FluentTest
+    {
+        public UnitTest1()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Firefox);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            I.Open("https://www.google.com/")
+                .Enter("skilltree").In("input[name='q']")
+                .Press("{ENTER}")
+                .Assert.Exists("a[href='https://skilltree.my/']");
         }
     }
 }
